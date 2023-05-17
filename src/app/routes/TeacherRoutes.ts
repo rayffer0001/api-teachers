cacdeb0d0a7e38e97149e522b20db9b6efa58bf3
rayffer0001 <rayffer@hotmail.es>
2023-05-16 20:48:29 -0500

    Creacion del controlador y las rutas para el profesor

--- src/app/routes/TeacherRoutes.ts
+++ src/app/routes/TeacherRoutes.ts
@@ -1,1 +1,26 @@
- 
+import { TeacherController } from './../controllers/TeacherController';
+import { TeacherRepository } from './../../infra/repositories/TeacherRepository';
+import { Router } from "express";
+
+export class TeacherRoutes {
+
+    private _router: Router;
+    private _teacherRepository: TeacherRepository;
+    private _teacherController: TeacherController;
+
+    constructor(){
+        this._router = Router();
+        this._teacherRepository = new TeacherRepository();
+        this._teacherController = new TeacherController(this._teacherRepository);
+        this.initRoute();
+    }
+
+    private initRoute(): void{  //inicializa las rutas
+        this._router.post('/create', this._teacherController.create.bind(this._teacherRepository)); //es hasta donde puedo acceder en una variable
+    }
+
+    public getRoutes(): Router {
+        return this._router;
+    }
+
+}
--- src/domain/entities/Teacher.ts
+++ src/domain/entities/Teacher.ts
@@ -1,1 +1,13 @@
- 
+export class Teacher {
+    
+    constructor(
+        public name: string,
+        public description: string,
+        public email: string,
+        public birthDate: Date,
+        public id?: string
+        ){
+
+        }
+
+} 
